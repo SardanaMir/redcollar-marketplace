267cef4792d0a06f50c1ac8573c5a6be4381f4af
SardanaMir <sardanadord@yandex.ru>
2024-02-07 08:02:13 +0300

    made swiper in category component

--- src/components/Categories/index.jsx
+++ src/components/Categories/index.jsx
@@ -1,19 +1,23 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useRef} from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
 import { useDispatch, useSelector } from 'react-redux'
 import { allCategories } from '../../api'
-import styles from './style.module.scss'
 import { setCategories } from '../../redux/slices/categoriesSlice'
+import 'swiper/css';
+
+import styles from './style.module.scss'
+
 
 const Categories = () => {
     const isActive = useSelector(state => state.search.isActive)
     const categoriesData = useSelector(state => state.categories.categories);
     const dispatch = useDispatch()
-    console.log('categories Data', categoriesData)
 
     useEffect(() => {
         const fetchCategories = async () => {
           try {
-            // const data = await allCategories();
+            const data = await allCategories();
             dispatch(setCategories(data));
             console.log('categories', data)
           } catch (error) {
@@ -23,22 +27,36 @@ const Categories = () => {
         fetchCategories();
     }, []);
 
+    const handleActive = (e) =>{
+        console.log(e.target)
+    }
+
   return (
-    
     <>
     {isActive ? (
         <></>
         ):(
-            <div className={styles.root}>
-                {
-                    categoriesData.map(category => (
-                        <div className={styles.category}>
-                            {category}
-                        </div>
-                    ))
-                }
-            </div>
+        <div className={styles.container}>
+        <Swiper
+            spaceBetween={1}
+            slidesPerView={9}
+            onSlideChange={(swiper) => (console.log('slide change'))}
+            onSwiper={(swiper) => console.log(swiper)}
+            >
+            {
+                categoriesData.map(category => (
+                <SwiperSlide>
+                    <div onClick={(e)=>handleActive(e)} className={styles.category}>
+                    {category}
+                    </div>
+                </SwiperSlide>
+                ))
+            }
+        </Swiper>
+        </div>
+
         )
+
     }
     </>
   )
